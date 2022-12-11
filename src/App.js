3ef8f62627218e26aee6c5d7c8c624ca773d9d3f
aly raafat <alewaraafat@gmail.com>
2022-12-12 01:55:32 +0200

    chat body and chat message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { React, useState, useEffect } from "react";
 import Sidebar from './Components/Sidebar/Sidebar';
+import Chat from './Components/Chat/Chat';
 function App() {
   return (
     <div className='app'>
       <div className='app__body'>
         <Sidebar/>
+        <Chat/>
       </div>
       
     </div>
--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,9 +1,43 @@
+import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
+import { SearchOutlined } from '@mui/icons-material';
+import AttachFile from '@mui/icons-material/AttachFile';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import './Chat.css';
 
 function Chat() {
   return (
     <div className='chat'>
-      
+      <div className='chat__header'>
+        <Avatar/>
+        <div className='chat__headerInfo'>
+          <h3>Room name</h3>
+          <p> Last seen at ....</p>
+        </div>
+        <div className='chat_headerRight'>
+          <IconButton>
+              <SearchOutlined/>
+          </IconButton>
+          <IconButton>
+              <AttachFile/>
+          </IconButton>
+          <IconButton>
+              <MoreVertIcon/>
+          </IconButton>
+        </div>
+      </div>
+      <div className='chat__body'>
+        <p className='chat__message'>
+          <span className='chat__name'>Aly</span>
+          This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+        <p className='chat__message chat__receiver'>
+          <span className='chat__name'>Aly</span>
+          This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+      </div>
     </div>
   )
 }
